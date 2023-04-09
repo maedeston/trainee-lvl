569e310b1905fc0647d233a27cfd2acba911efdf
leonid2323 <107189981+maedeston@users.noreply.github.com>
2023-04-09 14:41:36 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -30,6 +30,9 @@ class Card(Bank_product):
     card_valid_until = Column(Integer, nullable=False)
     card_owner = Column(String, nullable=False)
     card_balance = Column(Integer)
+    cvv = Column(Integer, unique=True, nullable=False)
+    code = Column(Integer, unique=True, nullable=False)
+    data = Column(Integer, nullable=False)
 
 class BC_pshysique_litso(Bank_Client):
     __tablename__ = 'bc_pshysique_litso'
@@ -47,7 +50,7 @@ class Account(Base):
     __table_args__ = {'extand_existing': True}
 
     account_id = Column(Integer, primary_key=True, nullable=False)
-    account_balance = Column(Float, nullable=False)
+    account_balance = Column(Float)
 
 class Transaction(Base):
     __tablename__ = 'transactions'
